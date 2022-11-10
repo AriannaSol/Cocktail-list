475836cdad6443ac262c8e4f81d9ada2bb21beb3
AriannaSol <ariannasolipaca@gmail.com>
2022-11-10 13:04:08 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
+        consequuntur. Aliquam accusantium eligendi perspiciatis minus porro,
+        qui, quis quas fugiat quo beatae ad sapiente, veniam ex. Inventore iure
+        cumque autem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
